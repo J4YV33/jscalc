2f28337e97ddb5a157fdbadb24a211822b9a60ca
Jay <87533912+J4YV33@users.noreply.github.com>
2022-10-29 14:15:57 -0500

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [display, setDisplay] = useState('0');
+
+    const handleNumber = (e) => {
+        const number = e.target.textContent;
+        setDisplay(display === '0' ? number : display + number);
+    };
+
+    const handleOperator = (e) => {
+        const operator = e.target.textContent;
+        setDisplay(display + '' + operator + '');
+    };
+
+    const handleEqual = () => {
+        setDisplay(eval(display));
+    };
+
+    const handleClear = () => {
+        setDisplay('0');
+    };
+
+    const handleDecimal = () => {
+        setDisplay(display + '.');
+    };
+
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div id="display" className="row">{display}</div>
+                <div id="clear" className="row" onClick={handleClear}> AC </div>
+                <div id="seven" onClick={handleNumber}>7</div>
+                <div id="eight" onClick={handleNumber}>8</div>
+                <div id="nine" onClick={handleNumber}>9</div>
+                <div id="multiply" onClick={handleOperator}>*</div>
+                <div id="four" onClick={handleNumber}>4</div>
+                <div id="five" onClick={handleNumber}>5</div>
+                <div id="six" onClick={handleNumber}>6</div>
+                <div id="divide" onClick={handleOperator}>/</div>
+                <div id="one" onClick={handleNumber}>1</div>
+                <div id="two" onClick={handleNumber}>2</div>
+                <div id="three" onClick={handleNumber}>3</div>
+                <div id="add" onClick={handleOperator}>+</div>
+                <div id="zero" onClick={handleNumber}>0</div>
+                <div id="decimal" onClick={handleDecimal}>.</div>
+                <div id="equals" onClick={handleEqual}>=</div>
+                <div id="subtract" onClick={handleOperator}>-</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
